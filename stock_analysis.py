cf29258a054f3db260abb436e59320993f735f75
KelvinPuyam <kpuyam@gmail.com>
2024-04-15 21:02:55 +0530

    Add docstring to stock_analysis script

--- stock_analysis.py
+++ stock_analysis.py
@@ -10,65 +10,118 @@ import io
 
 yf.pdr_override()
 
-df = pd.DataFrame()
+def get_stock_historical_info(stock, start_date, end_date):
+    """
+    Fetches historical stock data for a given stock symbol and date range.
 
-def get_stock_historical_info(stock , start_date, end_date):
-	if isinstance(start_date, str):
-		start_date = datetime.strptime(start_date, '%Y-%m-%d')
-		
-	if isinstance(end_date, str):
-		end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    Parameters:
+    - stock (str): Stock symbol.
+    - start_date (str or datetime): Start date in 'YYYY-MM-DD' format or as a datetime object.
+    - end_date (str or datetime): End date in 'YYYY-MM-DD' format or as a datetime object.
 
-	stock_value = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
-	return stock_value
+    Returns:
+    pandas.DataFrame: DataFrame containing the historical stock data.
+    """
+    if isinstance(start_date, str):
+        start_date = datetime.strptime(start_date, '%Y-%m-%d')
+        
+    if isinstance(end_date, str):
+        end_date = datetime.strptime(end_date, '%Y-%m-%d')
+
+    stock_value = pdr.get_data_yahoo(stock, start=start_date, end=end_date)
+    return stock_value
 
 def get_stock_data(stock, period="max"):
-	stock_info = yf.Ticker(stock)
-	all_data = stock_info.history(period=period, auto_adjust=True)
-	return all_data
+    """
+    Fetches historical stock data for a given stock symbol.
+
+    Parameters:
+    - stock (str): Stock symbol.
+    - period (str): Period for which historical data should be fetched. Default is "max".
+
+    Returns:
+    pandas.DataFrame: DataFrame containing the historical stock data.
+    """
+    stock_info = yf.Ticker(stock)
+    all_data = stock_info.history(period=period, auto_adjust=True)
+    return all_data
 
 def create_prophet_model(ticker, hist='max'):
-	# create new data frame to hold dates (ds) & adjusted closing prices (y)
-	stock_data = yf.Ticker(ticker)
+    """
+    Creates a Prophet model for stock forecasting based on historical stock data.
 
-	hist_data = stock_data.history(hist, auto_adjust=True)
+    Parameters:
+    - ticker (str): Stock ticker symbol.
+    - hist (str): Historical period for training the model. Default is 'max'.
 
-	df = pd.DataFrame()
+    Returns:
+    prophet.forecaster.Prophet: Prophet model.
+    """
+    stock_data = yf.Ticker(ticker)
+    hist_data = stock_data.history(hist, auto_adjust=True)
 
-	df['ds'] = hist_data.index.values
-	df['y'] = hist_data['Close'].values
+    df = pd.DataFrame()
+    df['ds'] = hist_data.index.values
+    df['y'] = hist_data['Close'].values
 
-	# create a Prophet model from that data
-	model = Prophet(
-		changepoint_prior_scale=0.05,
-		holidays_prior_scale=15,
-		seasonality_prior_scale=10,
-		weekly_seasonality=True,
-		yearly_seasonality=True,
-		daily_seasonality=False
-	)
+    model = Prophet(
+        changepoint_prior_scale=0.05,
+        holidays_prior_scale=15,
+        seasonality_prior_scale=10,
+        weekly_seasonality=True,
+        yearly_seasonality=True,
+        daily_seasonality=False
+    )
 
-	country = get_country_from_ticker(ticker)
-	model.add_country_holidays(country_name=country)
-	model.fit(df)
+    country = get_country_from_ticker(ticker)
+    model.add_country_holidays(country_name=country)
+    model.fit(df)
 
-	return model
+    return model
 
 def make_forecast(model, periods):
-	future = model.make_future_dataframe(periods, freq='D')
-	forecast = model.predict(future)
-	return forecast
+    """
+    Generates a forecast using the provided Prophet model for a specified number of future periods.
+
+    Parameters:
+    - model (prophet.forecaster.Prophet): Prophet model.
+    - periods (int): Number of future periods to forecast.
+
+    Returns:
+    pandas.DataFrame: DataFrame containing the forecast data.
+    """
+    future = model.make_future_dataframe(periods, freq='D')
+    forecast = model.predict(future)
+    return forecast
 
 def make_forecast_chart(model, forecast):
-	fig = model.plot_components(forecast)
-	#Save the figure to a BytesIO object
-	buf = io.BytesIO()
-	plt.savefig(buf, format='png')
-	plt.close(fig)  # Close the figure to free up memory
-	buf.seek(0)
-	return send_file(buf, mimetype='image/png')
+    """
+    Generates and returns a chart visualizing the forecast produced by the Prophet model.
+
+    Parameters:
+    - model (prophet.forecaster.Prophet): Prophet model.
+    - forecast (pandas.DataFrame): DataFrame containing the forecast data.
+
+    Returns:
+    flask.Response: Flask response containing the forecast chart as a PNG image.
+    """
+    fig = model.plot_components(forecast)
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png')
+    plt.close(fig)
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
 
 def get_country_from_ticker(ticker):
+    """
+    Determines the country associated with a given stock ticker symbol.
+
+    Parameters:
+    - ticker (str): Stock ticker symbol.
+
+    Returns:
+    str: Country code ('US' for United States, 'CA' for Canada, 'TR' for Turkey).
+    """
     if '.IS' in ticker:
         return 'TR'
     elif '.TO' in ticker:
